9eb31e9e84d4210706807e1a08550f875686679d
Chang Sau Sheong <sausheong@gmail.com>
2014-05-07 22:07:14 +0800

    cleaned up setup script

--- setup/setup.go
+++ setup/setup.go
@@ -5,8 +5,9 @@ import (
 )
 
 func main() {
+  gdb.DB.Exec("DROP TABLE users;DROP TABLE sessions;")
   gdb.DB.AutoMigrate(gdb.User{})
   gdb.DB.AutoMigrate(gdb.Session{})
-  user := gdb.User{Name: "Sau Sheong", Email: "sausheong@gmail.com", Password: "123"}
+  user := gdb.User{Name: "Admin", Email: "admin@goauthserv", Password: "admin"}
   gdb.DB.Save(&user)
 }
